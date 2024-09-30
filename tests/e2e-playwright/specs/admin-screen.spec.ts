6b64bf541cf5193c5803ec5eb87195f2c105821a
Dovid Levine <david@axepress.dev>
2024-09-30 21:32:13 +0000

    ci: fix commands

--- tests/e2e-playwright/specs/admin-screen.spec.ts
+++ tests/e2e-playwright/specs/admin-screen.spec.ts
@@ -3,8 +3,6 @@
  */
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 
-const timeout = 10000;
-
 test.describe( 'Plugin Admin Screen', () => {
 	test( 'Admin screen is present', async ( { page, admin } ) => {
 		await admin.visitAdminPage( 'admin.php?page=snapwp-helper' );
