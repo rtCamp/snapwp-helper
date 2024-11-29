d0691396965d2096638614ecf5b257aec5d969b5
Dovid Levine <david@axepress.dev>
2024-11-29 12:13:53 +0200

    chore: lint

--- src/Modules/GraphQL/Type/Fields/EnqueuedScript.php
+++ src/Modules/GraphQL/Type/Fields/EnqueuedScript.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Registers custom fields to the GraphQL EnqueuedScript Object.
- * 
+ *
  * Temporary (non-conflicting) backport until supported by WPGraphQL.
  *
- * @see https://github.com/wp-graphql/wp-graphql/pull/3196 
+ * @see https://github.com/wp-graphql/wp-graphql/pull/3196
  *
  * @package SnapWP\Helper\Modules\GraphQL\Type\Fields
  * @since 0.0.1
@@ -34,12 +34,12 @@ final class EnqueuedScript extends AbstractFields {
 				'type'        => 'String',
 				'description' => __( 'The location where this script should be loaded', 'snapwp-helper' ),
 				'resolve'     => static function ( \_WP_Dependency $script ) {
-					if ( isset( $script->extra['group'] ) && (int) $script->extra['group'] === 1 ) {
+					if ( isset( $script->extra['group'] ) && 1 === (int) $script->extra['group'] ) {
 						return 'header';
 					}
 
 					return 'footer';
-				}
+				},
 			],
 		];
 	}
