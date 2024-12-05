211ba3ea1d4b5086b95910c3c47d3e1ef3e11e80
Dovid Levine <david@axepress.dev>
2024-12-05 23:05:38 +0200

    ci: improve port/socket .env handling

--- bin/_lib.sh
+++ bin/_lib.sh
@@ -57,19 +57,14 @@ install_db() {
 		return 0
 	fi
 
-	# parse DB_HOST for port or socket references
-	local PARTS=(${WORDPRESS_DB_HOST//\:/ })
-	local DB_HOSTNAME=${PARTS[0]}
-	local DB_SOCK_OR_PORT=${PARTS[1]}
 	local EXTRA=""
 
-	if ! [ -z $DB_HOSTNAME ]; then
-		if [ $(echo $DB_SOCK_OR_PORT | grep -e '^[0-9]\{1,\}$') ]; then
-			EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
-		elif ! [ -z $DB_SOCK_OR_PORT ]; then
-			EXTRA=" --socket=$DB_SOCK_OR_PORT"
-		elif ! [ -z $DB_HOSTNAME ]; then
-			EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
+	if [ -n "$WORDPRESS_DB_SOCKET" ]; then
+		EXTRA=" --socket=$WORDPRESS_DB_SOCKET"
+	elif [ -n "$WORDPRESS_DB_HOST" ]; then
+		EXTRA=" --host=$WORDPRESS_DB_HOST" --protocol=tcp"
+		if [ -n "$WORDPRESS_DB_PORT" ]; then
+				EXTRA="$EXTRA --port=$WORDPRESS_DB_PORT"
 		fi
 	fi
 
