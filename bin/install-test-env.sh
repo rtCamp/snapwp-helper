64ebd38d5c227f90c7d092916148a52188597d38
Dovid Levine <david@axepress.dev>
2026-01-13 22:29:03 +0200

    chore: update deps (#122)
    * chore: update deps
    
    * chore: fix ci
    
    * chore: try disabling via docker
    
    * chore: fix echo
    
    * ci: use `--ssl=false`
    
    * .
    
    * ci: Try pinned version
    
    * ci: try again
    
    * ci: --skip-ssl
    
    * chore: fml

--- bin/install-test-env.sh
+++ bin/install-test-env.sh
@@ -27,7 +27,7 @@ cd "$WORDPRESS_ROOT_DIR" || { echo "Failed to enter directory: $WORDPRESS_ROOT_D
 # dirty up the tests.
 if [ "$1" == '--reset-site' ]; then
 	echo -e "$(status_message "Resetting test database...")"
-	wp db reset --yes --quiet --allow-root
+	wp db reset --yes --quiet --allow-root --defaults
 fi
 
 if [[ -f "wp-load.php" ]]; then
@@ -129,12 +129,12 @@ if [ "$SCRIPT_DEBUG" != "$SCRIPT_DEBUG_CURRENT" ]; then
 	echo -e "$(status_message "SCRIPT_DEBUG: $SCRIPT_DEBUG_RESULT...")"
 fi
 
-SQLDUMP="$WORDPRESS_ROOT_DIR/wp-content/plugins/$PLUGIN_SLUG/tests/_data/dump.sql" 
+SQLDUMP="$WORDPRESS_ROOT_DIR/wp-content/plugins/$PLUGIN_SLUG/tests/_data/dump.sql"
 mkdir -p "$(dirname "$SQLDUMP")"
 if [ ! -f "$SQLDUMP" ]; then
 	echo -e "$(status_message "Exporting test database dump...")"
 
-	wp db export "$SQLDUMP" --allow-root
+	wp db export "$SQLDUMP" --allow-root --defaults
 fi
 
 # Proof it all worked.
